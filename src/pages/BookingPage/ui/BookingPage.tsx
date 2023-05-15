0941a5646e57f1598588fb2f5ae5d8417c9aa739
Rariramz <mary4798713@gmail.com>
2023-05-15 20:00:58 +0300

    Cols added

--- src/pages/BookingPage/ui/BookingPage.tsx
+++ src/pages/BookingPage/ui/BookingPage.tsx
@@ -1,11 +1,19 @@
 import { classNames } from '../../../shared/lib/classNames/classNames';
-import cls from './BookingPage.module.scss';
 import { ReactCalendar } from '../../../widgets/ReactCalendar/ui/ReactCalendar';
+import { Col, ColGapSize } from '../../../shared/ui/Col/Col';
+import { Heading } from '../../../shared/ui/Heading/Heading';
+import cls from './BookingPage.module.scss';
 
 const ExplorePage = () => {
   return (
     <div className={classNames(cls.explorePage)}>
-      <ReactCalendar />
+      <Col className={cls.leftCol} gapSize={ColGapSize.XXL}>
+        Booking Page
+      </Col>
+      <Col className={cls.rightCol} gapSize={ColGapSize.XXL}>
+        <Heading>Calendar</Heading>
+        <ReactCalendar />
+      </Col>
     </div>
   );
 };
