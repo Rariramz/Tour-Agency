776192e895103f5955186279d36ec0fd241db903
Rariramz <mary4798713@gmail.com>
2023-05-27 01:53:08 +0300

    Navbar draft

--- frontend/src/pages/HomePage/ui/HomePage.tsx
+++ frontend/src/pages/HomePage/ui/HomePage.tsx
@@ -1,21 +1,26 @@
-import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 import { classNames } from '../../../shared/lib/classNames/classNames';
 import { Col, ColGapSize } from '../../../shared/ui/Col/Col';
 import { Heading } from '../../../shared/ui/Heading/Heading';
 import { DoneTours } from '../../../widgets/DoneTourCard';
+import { Navbar } from '../../../widgets/Navbar';
 import cls from './HomePage.module.scss';
 
 const HomePage = () => {
-  const { t } = useTranslation('main');
-
   return (
     <div className={classNames(cls.homePage)}>
-      <Col className={cls.DoneTripsCol} gapSize={ColGapSize.XXL}>
-        <Heading>Done trips</Heading>
-        <DoneTours />
-      </Col>
-      <Col className={cls.UpcomingTrips} gapSize={ColGapSize.XXL}>
-        <Heading>Upcoming trips</Heading>
+      <Col className={classNames(cls.homePageCol)}>
+
+        <Navbar />
+
+        <Col className={cls.DoneTripsCol} gapSize={ColGapSize.XXL}>
+          <Heading>Done trips</Heading>
+          <DoneTours />
+        </Col>
+        <Col className={cls.UpcomingTrips} gapSize={ColGapSize.XXL}>
+          <Heading>Upcoming trips</Heading>
+        </Col>
+
       </Col>
     </div>
   );
---
+++ frontend/src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Col } from '../../../shared/ui/Col/Col';
+import { Heading } from '../../../shared/ui/Heading/Heading';
+import { Row, RowAlign } from '../../../shared/ui/Row/Row';
+import { Paragraph } from '../../../shared/ui/Paragraph/Paragraph';
+import { Search } from '../../../features/search/Search/ui/Search';
+import { IconBg } from '../../../shared/ui/IconBg/IconBg';
+import NotificationIcon from '../../../shared/assets/notification.svg';
+import AvatarIcon from '../../../shared/assets/avatar.svg';
+import cls from './Navbar.module.scss';
+
+const Navbar = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  return (
+    <Row align={RowAlign.BETWEEN}>
+        <Col className={cls.navbarWelcomeCol}>
+        <Heading className={cls.navbarWelcomeName}>Hello Maria!</Heading>
+        <Paragraph className={cls.navbarWelcomeText}>Welcome back and explore new trips</Paragraph>
+        </Col>
+        <Search className={cls.navbarSearch} value={searchValue} onChange={setSearchValue} />
+        <Row className={cls.navbarAccountIconsRow}>
+        <IconBg className={cls.navbarIconBg}><NotificationIcon/></IconBg>
+        <AvatarIcon className={cls.navbarAvatar} />
+        </Row>
+    </Row>
+  );
+};
+
+export { Navbar };
