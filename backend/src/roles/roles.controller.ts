c3f7ba9b597ed0f707c679362fe6b1ae40b548e6
Rariramz <mary4798713@gmail.com>
2023-06-17 15:11:52 +0300

    roles endpoint swagger

--- backend/src/roles/roles.controller.ts
+++ backend/src/roles/roles.controller.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('roles')
 export class RolesController {
   constructor(private rolesService: RolesService) {}
 
+  
+  @ApiOperation({ summary: 'Role creation' })
+  @ApiResponse( { status: 200 })
   @Post()
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.rolesService.createRole(createRoleDto);
   }
   
+  @ApiOperation({ summary: 'Getting role by name' })
+  @ApiResponse( { status: 200 })
   @Get('/:name')
   getByName(@Param('name') name: string) {
     return this.rolesService.getRoleByName(name);
